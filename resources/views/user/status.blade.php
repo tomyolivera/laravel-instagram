583e2c44ac8ac4851b5f53d93c74b293ee291efc
tomyolivera <72465832+tomyolivera@users.noreply.github.com>
2021-03-16 08:38:21 -0300

    Update status.blade.php

--- resources/views/user/status.blade.php
+++ resources/views/user/status.blade.php
@@ -1,10 +1,10 @@
-//@if (Auth::user()->status == 0)
-//    <div class='flex align-center justify-center'><i class='text-gray-500'>fiber_manual_record</i> <span>{{ __('Offline') }}</span></div>
-//@elseif (Auth::user()->status == 1)
-//    <div class='flex align-center justify-center'><i class='text-green-500'>fiber_manual_record</i> <span>{{ __('Online') }}</span></div>
-//@elseif (Auth::user()->status == 2)
-//    <div class='flex align-center justify-center'><i class='text-yellow-500'>fiber_manual_record</i> <span>{{ __('Busy') }}</span></div>
-//@endif
+@if (Auth::user()->status == 0)
+    <div class='flex align-center justify-center'><i class='text-gray-500'>fiber_manual_record</i> <span>{{ __('Offline') }}</span></div>
+@elseif (Auth::user()->status == 1)
+    <div class='flex align-center justify-center'><i class='text-green-500'>fiber_manual_record</i> <span>{{ __('Online') }}</span></div>
+@elseif (Auth::user()->status == 2)
+    <div class='flex align-center justify-center'><i class='text-yellow-500'>fiber_manual_record</i> <span>{{ __('Busy') }}</span></div>
+@endif
 
 @switch(Auth::user()->status)
     @case(0)
